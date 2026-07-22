62bcb9e0fba050293f0cae32fed073b4d17f0b4c
Agent 22 <agent22@goodstorytime.com>
2026-07-22 19:33:45 +0700

    fix(webui): ignore .svelte-kit directory

--- web/.svelte-kit/output/server/nodes/1.js
+++
@@ -1,6 +0,0 @@
-
-
-export const index = 1;
-export const imports = ["_app/immutable/nodes/1.CANAV2ea.js","_app/immutable/chunks/CV8GLy5s.js","_app/immutable/chunks/BVaEnE3R.js","_app/immutable/chunks/CsbLMh5S.js","_app/immutable/chunks/CuDynTWb.js","_app/immutable/chunks/OA8UcISh.js"];
-export const stylesheets = [];
-export const fonts = [];
